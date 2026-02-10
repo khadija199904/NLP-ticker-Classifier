9377224871d940fce2123382c195a2fd89beaec9
Khadija199904 <khadijaelabbioui1999@gmail.com>
2026-02-10 18:21:12 +0100

    NTC-9: Configuration de Chromadb

--- config.py
+++ config.py
@@ -7,4 +7,9 @@ load_dotenv()
 # Configuration des modèles
 EMBEDDING_MODEL_NAME = os.getenv("EMBEDDING_MODEL_NAME", "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2")
 HF_TOKEN = os.getenv("HF_TOKEN")
+DATA_CLEANED_PATH = os.getenv("DATA_CLEANED_PATH", "data/data_processed.csv")
 
+# CHROMA CONFIGURATION
+CHROMA_HOST = os.getenv("CHROMA_HOST", "localhost")
+CHROMA_PORT = os.getenv("CHROMA_PORT", "8080")
+COLLECTION_NAME = os.getenv("CHROMA_COLLECTION_NAME", "tickets_collection")
