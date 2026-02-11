747324eef1d472c54defb0d70d178d733cdbd641
Khadija199904 <khadijaelabbioui1999@gmail.com>
2026-02-11 20:45:48 +0100

    NTC-9: add test for connexion chroma db

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 import os
 from dotenv import load_dotenv
+import chromadb
+
 
 
 load_dotenv()
@@ -7,9 +9,19 @@ load_dotenv()
 # Configuration des modèles
 EMBEDDING_MODEL_NAME = os.getenv("EMBEDDING_MODEL_NAME", "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2")
 HF_TOKEN = os.getenv("HF_TOKEN")
-DATA_CLEANED_PATH = os.getenv("DATA_CLEANED_PATH", "data/data_processed.csv")
+DATA_CLEANED_PATH = os.getenv("DATA_CLEANED_PATH", "data/data_prepared.csv")
 
 # CHROMA CONFIGURATION
 CHROMA_HOST = os.getenv("CHROMA_HOST", "localhost")
 CHROMA_PORT = os.getenv("CHROMA_PORT", "8080")
 COLLECTION_NAME = os.getenv("CHROMA_COLLECTION_NAME", "tickets_collection")
+
+if __name__ == "__main__":
+  try:
+    print(f"Tentative de connexion à ChromaDB sur {CHROMA_HOST}:{CHROMA_PORT}...")
+    client = chromadb.HttpClient(host=CHROMA_HOST, port=int(CHROMA_PORT))
+    heartbeat = client.heartbeat()
+    print(f"✅ Succès ! Serveur opérationnel (Heartbeat: {heartbeat})")
+  except Exception as e:
+    print(f"❌ Erreur de connexion : {e}")
+    print("Assurez-vous que Docker Desktop ou le conteneur ChromaDB est démarré.")
